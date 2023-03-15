27a749f94f257a868ced9096e58bbb57e01822b2
TheKatsu1 <xjverudonx@gmail.com>
2023-03-15 20:42:14 -0300

    Criação da classe cliente

---
+++ ControleContas/Model/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleContas.Model
+{
+    public class Cliente
+    {
+        public string Nome { get; private set; }
+        public string Cpf { get; private set; }
+        public int AnoNascimento { get; private set; }
+
+    }
+}
--- ControleContas/Program.cs
+++ ControleContas/Program.cs
@@ -1,6 +1,8 @@
-﻿
-using ControleContas.Model;
+﻿using ControleContas.Model;
 
-Conta conta = new Conta("1234");
-Conta conta2 = new Conta("4321", 1000.99m);
+Conta conta = new Conta("1234" , 1000.99m);
+Conta conta2 = new Conta("654321", 2341.42m);
 Console.WriteLine($"Conta: {conta.Numero}; Saldo: {conta.Saldo}");
+Console.WriteLine($"Conta: {conta2.Numero}; Saldo: {conta2.Saldo}");
+Console.WriteLine($"Conta: {conta.Saldo + conta2.Saldo}");
+
